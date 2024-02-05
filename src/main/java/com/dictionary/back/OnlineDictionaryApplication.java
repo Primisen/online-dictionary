ddbdd99e0058852a82c4877e31dcb4ed91125582
Nadzeya <primisen17@gmail.com>
2024-02-05 11:41:31 +0100

    Init commit

---
+++ src/main/java/com/dictionary/back/OnlineDictionaryApplication.java
@@ -0,0 +1,13 @@
+package com.dictionary.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineDictionaryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineDictionaryApplication.class, args);
+    }
+
+}
